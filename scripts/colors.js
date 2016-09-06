7fe283eb07b3192a98d0888c20f3190245a13bc6
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-06 15:17:05 -0500

    v2 remove button recreation on each display update

--- scripts/colors.js
+++ scripts/colors.js
@@ -12,34 +12,46 @@ $(document).ready(function () {
   console.log('jq working');
   //clear loading messages
   $('#outDiv').empty();
-  //and update counters & buttons
-  updateTopDisp();
+  $('#countDiv').empty();
+  $('#btnDiv').empty();
+  //add counters and buttons for each color
+  for (var i = 0; i < colors.length; i++) {
+    $('#countDiv').append('<p color="'+ i +'">Total ' + colors[i].color + ': 0</p>');
+    $('#btnDiv').append('<button class="colorBtn" color="'+ i +'">' + colors[i].color[0].toUpperCase() + colors[i].color.slice(1) + '</button>');
+  }//end for
+  //add function to each button that will create a color square and update that color's counter
   $('body').on('click', '.colorBtn', function(){
     // console.log('in color button, this color\'s index=' + $(this).attr('color'));
     var color = $(this).attr('color')
     colors[color].count++;
-    updateTopDisp();
+    updateCounts();
     addColorDiv(color);
-  })
+  })// end .colorBtn onclick
+  //add function to each color square that will remove the square and update that color's counter
   $('body').on('click', '.colorDiv', function(){
     $(this).remove();
     colors[$(this).attr('color')].count--;
-    updateTopDisp();
-  })
-});
+    updateCounts();
+  })//end .colorDiv onclick
+});//end doc ready
 
-var updateTopDisp = function(){
-  $('#countDiv').empty();
-  $('#btnDiv').empty();
-  for (var i = 0; i < colors.length; i++) {
-    $('#countDiv').append('<p color="'+ i +'">Total ' + colors[i].color + ': '+ colors[i].count +'</p>');
-    $('#btnDiv').append('<button class="colorBtn" color="'+ i +'">' + colors[i].color[0].toUpperCase() + colors[i].color.slice(1) + '</button>');
-  }//end for
+//playing a little with selectors and String.slice here
+//changes each #countDiv <p> html string to that string with it's last index updated to the proper color count
+var updateCounts = function(){
+  for (var i = 0; i < $('#countDiv p').length; i++) {
+    $('#countDiv p[color="'+ i + '"]').html(
+      $('#countDiv p[color="'+ i + '"]').html().slice(0, $('#countDiv p[color="'+ i + '"]').html().length - 1) + colors[i].count
+    );
+  }
 }
 
 var addColorDiv = function(color){
+  //make new jQuery div so I can play with .attr() a bit
   var newDiv = $('<div class="colorDiv"/>');
+  //keep color attribute so counts can be updated properly
   newDiv.attr('color', color)
+  //style the color divs in jQuery because I can
   newDiv.attr('style', 'width: 100px; height: 100px; display: inline-block; border: 1px solid black; background-color:' + colors[color].color + ';');
+  //append the new div to #outDiv
   $('#outDiv').append(newDiv);
 };
